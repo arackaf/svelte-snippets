9cdafc68c4c371e0a87f7c82693d79c37d21af53
Adam Rackis <adamrackis@hotmail.com>
2024-07-27 19:15:32 -0500

    Work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
-<slot></slot>
+<div class="flex flex-col gap-4 p-4">
+	<div class="flex flex-row gap-3">
+		<a href="/snippets-for-reuse">Basic snippets</a>
+	</div>
+	<slot></slot>
+</div>
---
+++ src/routes/snippets-for-reuse/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	type Product = {
+		name: string;
+		url: string;
+		price: number;
+	};
+
+	let searchedBook = $state<Product>({
+		name: 'Effective TypeScript: 83 Specific Ways to Improve Your TypeScript, 2nd Edition',
+		url: 'https://m.media-amazon.com/images/I/71eWL4AqPqL._SL1500_.jpg',
+		price: 44.99
+	});
+	let recommendedBook = $state<Product>({
+		name: 'Modern C++ Design: Generic Programming and Design Patterns Applied',
+		url: 'https://m.media-amazon.com/images/I/914ncVx1hxL._SL1413_.jpg',
+		price: 55.49
+	});
+</script>
+
+{#snippet productDisplay(p: Product)}
+	<div class="flex flex-row gap-3">
+		<img class="max-w-[100px]" src={p.url} alt="product url" />
+		<div class="flex flex-col">
+			<h2 class="text-lg font-bold">{p.name}</h2>
+			<span class="italic">${p.price.toFixed(2)}</span>
+		</div>
+	</div>
+{/snippet}
+
+<section class="flex flex-col gap-5">
+	<h1 class="mb-5 text-3xl">Product Display Page</h1>
+
+	{@render productDisplay(searchedBook)}
+
+	<aside>You might also be interested in:</aside>
+
+	{@render productDisplay(recommendedBook)}
+</section>
