844345f1d24825bfa2663d2701db4116d46c4f39
Adam Rackis <adamrackis@hotmail.com>
2024-07-27 19:33:49 -0500

    Work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 <div class="flex flex-col gap-4 p-4">
 	<div class="flex flex-row gap-3">
 		<a href="/snippets-for-reuse">Basic snippets</a>
+		<a href="/injected-snippets">Injected snippets</a>
 	</div>
 	<slot></slot>
 </div>
---
+++ src/routes/injected-snippets/DisplayProduct.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { Product } from './types';
+
+	type Props = {
+		product: Product;
+		relatedProduct?: Product;
+		productDisplay: Snippet<[Product]>;
+		children: Snippet;
+	};
+
+	let { product, relatedProduct, productDisplay, children }: Props = $props();
+</script>
+
+<section class="flex flex-col gap-5">
+	{@render children()}
+
+	{@render productDisplay(product)}
+
+	{#if relatedProduct}
+		<aside>You might also be interested in:</aside>
+		{@render productDisplay(relatedProduct)}
+	{/if}
+</section>
